a93516793b3597a265cc066a0fac219b3cd70313
AyelenIasich <ayeleniasich@gmail.com>
2025-01-06 21:15:10 -0300

    Refactor reducer: added object-based implementation and improved comments

--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,10 +1,13 @@
 import { SET_POKEMONS } from "../actions/types";
 
+// Initial state of the reducer
 const initialState = {
   pokemons: [],
 };
 
-export const pokemonsReducer = (state = initialState, action) => {
+// Reducer using a switch statement
+// Handles actions based on their type
+export const pokemonsReducer1 = (state = initialState, action) => {
   switch (action.type) {
     case SET_POKEMONS:
       return { ...state, pokemons: action.payload };
@@ -13,3 +16,13 @@ export const pokemonsReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Reducer using an object-based approach
+// Dynamically maps actions to state updates
+export const pokemonsReducer = (state = initialState , action)=>{
+  return reducerObject(state, action.payload)[action.type] || state;
+};
+
+const reducerObject = (state, payload) =>({
+  [SET_POKEMONS] : { ...state, pokemons: payload}
+})
